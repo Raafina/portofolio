8cdd69f3327624dfacb2eab2c6541feb69ded583
Raafina <raafinuradzani16@gmail.com>
2025-07-08 00:15:48 +0700

    update: techstack-badge on dark mode

--- src/ui/techstack-badge.tsx
+++ src/ui/techstack-badge.tsx
@@ -3,7 +3,7 @@ interface TechStackBadgeProps {
 }
 export default function TechStackBadge({ tech }: TechStackBadgeProps) {
   return (
-    <span className="inline-block bg-primary/10 text-primary px-2 py-1 rounded-full text-xs font-medium mr-2 mb-2">
+    <span className="inline-block bg-primary/10 text-primary dark:bg-primary dark:text-white px-2 py-1 rounded-full text-xs font-medium mr-2 mb-2">
       {tech}
     </span>
   );
